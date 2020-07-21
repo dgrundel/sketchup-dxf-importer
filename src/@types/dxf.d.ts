512bc6a847f71eebafc14173d7fa7d5972ee2fbb
grundel <daniel@grundel.me>
2020-07-21 16:25:17 -0700

    starting type def for dxf, show dxf as svg

---
+++ src/@types/dxf.d.ts
@@ -0,0 +1,6 @@
+declare module 'dxf' {
+    export class Helper {
+        constructor(contents: string);
+        toSVG(): string;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Helper } from 'dxf';
+
 interface Sketchup {
     getFile: () => void;
 }
@@ -14,8 +16,11 @@ class SketchUpConnector {
     }
 
     receiveFileContents(contents: string) {
-        const el = document.createElement('pre');
-        el.textContent = contents;
+        const helper = new Helper(contents);
+        const svg = helper.toSVG();
+
+        const el = document.createElement('svg');
+        el.innerHTML = svg;
         document.body.appendChild(el);
     }
 }
